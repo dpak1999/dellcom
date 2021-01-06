882a9a9d4bfb1209cb4f1103afcf9c05851141f0
dpak1999 <deepakkumardash313@gmail.com>
2021-01-06 19:36:58 +0530

    add : default state to home screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -4,7 +4,21 @@ import React, { useState, useEffect } from "react";
 import Product from "../Components/Product";
 
 const HomeScreen = () => {
-  const [localStorageProducts, setLocalStorageProducts] = useState([]);
+  const [localStorageProducts, setLocalStorageProducts] = useState([
+    {
+      id: 1,
+      name: "Airpods Wireless Bluetooth Headphones",
+      image: "/images/airpods.jpg",
+      description:
+        "Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working",
+      brand: "Apple",
+      category: "Electronics",
+      price: 89.99,
+      countInStock: 10,
+      rating: 4.5,
+      numReviews: 12,
+    },
+  ]);
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(e.target.id);
