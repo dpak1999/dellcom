3a7e965a9a5eb5d0d08cc9fd707856bfe0e2107c
dpak1999 <deepakkumardash313@gmail.com>
2021-01-12 19:01:26 +0530

    add : context api

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import NavbarComponent from "./Components/NavbarComponent";
+import ContextProvider from "./context";
 import AddProductScreen from "./screens/AddProductscreen";
 import CartScreen from "./screens/CartScreen";
 import HomeScreen from "./screens/HomeScreen";
@@ -11,14 +12,16 @@ import ProductListScreen from "./screens/ProductListScreen";
 
 function App() {
   return (
-    <Router>
-      <NavbarComponent />
-      <Route path="/" exact component={HomeScreen} />
-      <Route path="/backend" exact component={ProductListScreen} />
-      <Route path="/add" exact component={AddProductScreen} />
-      <Route path="/edit/:id" exact component={ProductEditScreen} />
-      <Route path="/cart" exact component={CartScreen} />
-    </Router>
+    <ContextProvider>
+      <Router>
+        <NavbarComponent />
+        <Route path="/" exact component={HomeScreen} />
+        <Route path="/backend" exact component={ProductListScreen} />
+        <Route path="/add" exact component={AddProductScreen} />
+        <Route path="/edit/:id" exact component={ProductEditScreen} />
+        <Route path="/cart" exact component={CartScreen} />
+      </Router>
+    </ContextProvider>
   );
 }
 
